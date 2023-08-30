41a212150d8b05887813c0632a6222ce0cb14acd
Tope-stack <yusufftope70@gmail.com>
2023-08-30 19:54:01 +0100

    User Auth Setup

---
+++ QuickTix.API/Entities/DTOs/UserLoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickTix.API.Entities.DTOs
+{
+    public class UserLoginDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; init; }
+        [Required(ErrorMessage = "Password is reuired")]
+        public string Password { get; init; }
+    }
+}
--- QuickTix.API/Repositories/Services/UserAuth.cs
+++ QuickTix.API/Repositories/Services/UserAuth.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using QuickTix.API.Entities;
 using QuickTix.API.Entities.DTOs;
 using QuickTix.API.Repositories.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace QuickTix.API.Repositories.Services
 {
@@ -19,11 +23,61 @@ namespace QuickTix.API.Repositories.Services
             _mapper = mapper;
             _configuration = configuration;
         }
+
         public async Task<IdentityResult> RegisterUserAsync(UserRegistrationDto userRegistration)
         {
             var user = _mapper.Map<ApplicationUser>(userRegistration);
             var result = await _userManager.CreateAsync(user, userRegistration.Password);
             return result;
+        }
+
+        public Task<bool> ValidateUserAsync(UserLoginDto userLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> CreateTokenAsync()
+        {
+            var signingCredentials = GetSigningCredentials();
+            var claims = await GetClaims();
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var jwtConfig = _configuration.GetSection("jwtConfig");
+            var key = Encoding.UTF8.GetBytes(jwtConfig["Secret"]);
+            var secret = new SymmetricSecurityKey(key);
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, _user.UserName)
+            };
+            var roles = await _userManager.GetRolesAsync(_user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("JwtConfig");
+            var tokenOptions = new JwtSecurityToken
+                (
+                issuer: jwtSettings["validIssuer"],
+                audience: jwtSettings["validAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expiresIn"])),
+                signingCredentials: signingCredentials
+                );
+            return tokenOptions;
         }
     }
 }
