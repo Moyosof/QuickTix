e6bb0623f4693dfbb74a3b47a6afa3fb80725dc1
Tope-stack <yusufftope70@gmail.com>
2023-09-02 00:00:07 +0100

    Email Sender Setup Done

--- QuickTix.API/Repositories/Services/EmailClientService.cs
+++ QuickTix.API/Repositories/Services/EmailClientService.cs
@@ -1,13 +1,50 @@
-﻿using QuickTix.API.Entities.Services;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using QuickTix.API.Entities.Services;
 using QuickTix.API.Repositories.Interfaces;
 
 namespace QuickTix.API.Repositories.Services
 {
     public class EmailClientService : IEmailClient
     {
-        public Task SendEmailAsync(MailRequestService mailRequest)
+        private readonly MailClientService _mailClientService;
+
+        public EmailClientService(IOptions<MailClientService> mailClientService)
         {
-            throw new NotImplementedException();
+            _mailClientService = mailClientService.Value;
+        }
+        public async Task SendEmailAsync(MailRequestService mailRequest)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailClientService.Email);
+            email.To.Add(MailboxAddress.Parse(mailRequest.To));
+            email.Subject = mailRequest.Subject;
+            var builder = new BodyBuilder();
+            if (mailRequest.Attachments != null)
+            {
+                byte[] fileBytes;
+                foreach (var file in mailRequest.Attachments) 
+                {
+                    if (file.Length > 0)
+                    {
+                        using (var ms = new  MemoryStream()) 
+                        {
+                            file.CopyTo(ms);
+                            fileBytes = ms.ToArray();
+                        }
+                        builder.Attachments.Add(file.FileName, fileBytes, ContentType.Parse(file.ContentType));
+                    }
+                }
+            }
+            builder.HtmlBody = mailRequest.Body;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailClientService.Host, _mailClientService.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailClientService.Email, _mailClientService.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
         }
     }
 }
